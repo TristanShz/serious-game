94bf51e01da5aa0ab32d0f916c1cef23cb2dfa9f
demme <demmerleraph@hotmail.com>
2022-08-31 08:37:02 +0200

    try to fix build bug

--- pages/formations/[categoryAlias]/[formationAlias].tsx
+++ pages/formations/[categoryAlias]/[formationAlias].tsx
@@ -10,73 +10,77 @@ import { formationsStore } from "../../../resources/formations/_stores/Formation
 import { TFormationMdl } from "../../../resources/formations/_models/FormationMdl";
 import { TCategoryMdl } from "../../../resources/formations/categories/_model/CategoryMdl";
 
-export const FormationAlias = (props: { formation: TFormationMdl, category: TCategoryMdl }) => {
-  return (
-    <div className={"w-screen flex flex-col"}>
-      <div className={"relative w-full h-[calc(80vh)] flex justify-center items-center "}>
-        <motion.div
-          initial={{ y: "-100vh" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 1 }}
-          className={
-            "absolute -z-20 inset-0 bg-cover bg-no-repeat bg-right-bottom bg-[url('/images/categories/categoryBg.png')]"
-          }
-        />
-        <motion.div
-          initial={{ y: "-100vh" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 1 }}
-          className={"absolute inset-0 -z-10 bg-black opacity-20"}
-        />
-        <TitleBlock white smallText title={props.formation.title} text={props.formation.description}
-                    className={"w-3/5"} regionSupport={props.formation.regionSupport} />
-        <Line color={"white"} scroll className={"absolute bottom-10 w-3/5"} />
-      </div>
-      <div className={"w-[45%] flex flex-col self-center"}>
-        
-      </div>
-    </div>
-  );
+export const FormationAlias = (props: { formation: TFormationMdl; category: TCategoryMdl }) => {
+    return (
+        <div className={"w-screen flex flex-col"}>
+            <div className={"relative w-full h-[calc(80vh)] flex justify-center items-center "}>
+                <motion.div
+                    initial={{ y: "-100vh" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                    className={
+                        "absolute -z-20 inset-0 bg-cover bg-no-repeat bg-right-bottom bg-[url('/images/categories/categoryBg.png')]"
+                    }
+                />
+                <motion.div
+                    initial={{ y: "-100vh" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                    className={"absolute inset-0 -z-10 bg-black opacity-20"}
+                />
+                <TitleBlock
+                    white
+                    smallText
+                    title={props.formation.title}
+                    text={props.formation.description}
+                    className={"w-3/5"}
+                    regionSupport={props.formation.regionSupport}
+                />
+                <Line color={"white"} scroll className={"absolute bottom-10 w-3/5"} />
+            </div>
+            <div className={"w-[45%] flex flex-col self-center"}></div>
+        </div>
+    );
 };
 
 FormationAlias.getLayout = function getLayout(page: ReactElement) {
-  return <RegularLayout headerProps={{ fixed: true }}>{page}</RegularLayout>;
+    return <RegularLayout headerProps={{ fixed: true }}>{page}</RegularLayout>;
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { items: categories } = await categoriesStore.list();
-  const { items: formations } = await formationsStore.list();
+    const { items: categories } = await categoriesStore.list();
+    const { items: formations } = await formationsStore.list();
 
-  return {
-    paths: formations.map((formation) => {
-      const categoryAlias = categories.find((category) => category._id === formation.category)!.urlAlias;
-      return {
-        params: {
-          categoryAlias,
-          formationAlias: formation.alias
-        }
-      };
-    }),
-    fallback: "blocking"
-  };
+    return {
+        paths: formations.map((formation) => {
+            const categoryAlias = categories.find((category) => category._id === formation.category)!.urlAlias;
+            return {
+                params: {
+                    categoryAlias,
+                    formationAlias: formation.alias,
+                },
+            };
+        }),
+        fallback: "blocking",
+    };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  if (context.params && typeof context.params.formationAlias === "string") {
-    const alias = context.params.formationAlias;
-    const { data: formation } = await formationsStore.getOneByAlias(alias);
+    if (context.params && typeof context.params.formationAlias === "string") {
+        const alias = context.params.formationAlias;
+        const { data: formation } = await formationsStore.getOneByAlias(alias);
 
-    return {
-      props: { formation },
-      revalidate: 10
-    };
-  } else {
-    return {
-      redirect: {
-        destination: pages.formations.path,
-        permanent: false
-      }
-    };
-  }
+        return {
+            props: { formation },
+            revalidate: 10,
+        };
+    } else {
+        return {
+            redirect: {
+                destination: pages.formations.path,
+                permanent: false,
+            },
+        };
+    }
 };
 export default FormationAlias;
--- pages/formations/[categoryAlias]/index.tsx
+++ pages/formations/[categoryAlias]/index.tsx
@@ -13,84 +13,84 @@ import { FormationBlock } from "../../../resources/formations/components/Formati
 import { TFormationMdl } from "../../../resources/formations/_models/FormationMdl";
 
 export const CategoryAlias = (props: { formations: TFormationMdl[]; category: TCategoryMdl }) => {
-  console.log(props.formations);
-  return (
-    <div className={"w-screen flex flex-col"}>
-      <div className={"relative w-full h-[calc(80vh)] flex justify-center items-center "}>
-        <motion.div
-          initial={{ y: "-100vh" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 1 }}
-          className={
-            "absolute -z-20 inset-0 bg-cover bg-no-repeat bg-right-bottom bg-[url('/images/categories/categoryBg.png')]"
-          }
-        />
-        <motion.div
-          initial={{ y: "-100vh" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 1 }}
-          className={"absolute inset-0 -z-10 bg-black opacity-20"}
-        />
-        <TitleBlock
-          white
-          smallText
-          title={props.category.pageTitle}
-          text={props.category.pageDescription}
-          className={"w-3/5"}
-        />
-        <Line color={"white"} scroll className={"absolute bottom-10 w-3/5"} />
-      </div>
-      <div className={"w-[45%] flex flex-col self-center"}>
-        <div className={"flex my-12 items-center gap-4"}>
-          <p className={"uppercase font-bold"}>Liste de nos formations</p>
-          <div className={"h-0.4 bg-black flex-1"} />
+    console.log(props.formations);
+    return (
+        <div className={"w-screen flex flex-col"}>
+            <div className={"relative w-full h-[calc(80vh)] flex justify-center items-center "}>
+                <motion.div
+                    initial={{ y: "-100vh" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                    className={
+                        "absolute -z-20 inset-0 bg-cover bg-no-repeat bg-right-bottom bg-[url('/images/categories/categoryBg.png')]"
+                    }
+                />
+                <motion.div
+                    initial={{ y: "-100vh" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 1 }}
+                    className={"absolute inset-0 -z-10 bg-black opacity-20"}
+                />
+                <TitleBlock
+                    white
+                    smallText
+                    title={props.category.pageTitle}
+                    text={props.category.pageDescription}
+                    className={"w-3/5"}
+                />
+                <Line color={"white"} scroll className={"absolute bottom-10 w-3/5"} />
+            </div>
+            <div className={"w-[45%] flex flex-col self-center"}>
+                <div className={"flex my-12 items-center gap-4"}>
+                    <p className={"uppercase font-bold"}>Liste de nos formations</p>
+                    <div className={"h-0.4 bg-black flex-1"} />
+                </div>
+                <FormationBlock
+                    {...props.formations[0]}
+                    redirectTo={pages.formation.path(props.category.urlAlias, props.formations[0].alias)}
+                />
+            </div>
         </div>
-        <FormationBlock
-          {...props.formations[0]}
-          redirectTo={pages.formation.path(props.category.urlAlias, props.formations[0].alias)}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 CategoryAlias.getLayout = function getLayout(page: ReactElement) {
-  return <RegularLayout headerProps={{ fixed: true }}>{page}</RegularLayout>;
+    return <RegularLayout headerProps={{ fixed: true }}>{page}</RegularLayout>;
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { items: categories } = await categoriesStore.list();
+    const { items: categories } = await categoriesStore.list();
 
-  return {
-    paths: categories.map((category) => {
-      return { params: { categoryAlias: category.urlAlias } };
-    }),
-    fallback: "blocking"
-  };
+    return {
+        paths: categories.map((category) => {
+            return { params: { categoryAlias: category.urlAlias } };
+        }),
+        fallback: "blocking",
+    };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  if (context.params && typeof context.params.categoryAlias === "string") {
-    const alias = context.params.categoryAlias;
-    const { data: category } = await categoriesStore.getOneByAlias(alias);
-    const { items: formations } = await formationsStore.listing(alias, undefined, undefined, undefined, [
-      {
-        id: "category",
-        type: TFilterType.ID,
-        value: category._id
-      }
-    ]);
-    return {
-      props: { formations, category },
-      revalidate: 10
-    };
-  } else {
-    return {
-      redirect: {
-        destination: pages.formations.path,
-        permanent: false
-      }
-    };
-  }
+    if (context.params && typeof context.params.categoryAlias === "string") {
+        const alias = context.params.categoryAlias;
+        const { data: category } = await categoriesStore.getOneByAlias(alias);
+        const { items: formations } = await formationsStore.listing(alias, undefined, undefined, undefined, [
+            {
+                id: "category",
+                type: TFilterType.ID,
+                value: category._id,
+            },
+        ]);
+        return {
+            props: { formations, category },
+            revalidate: 10,
+        };
+    } else {
+        return {
+            redirect: {
+                destination: pages.formations.path,
+                permanent: false,
+            },
+        };
+    }
 };
 export default CategoryAlias;
