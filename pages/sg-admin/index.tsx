352f0386a5385c06a975bf0a89f2dbd4c8e39823
demme <demmerleraph@hotmail.com>
2022-09-06 12:45:52 +0200

    add quizz table in admin dashboard

--- pages/sg-admin/index.tsx
+++ pages/sg-admin/index.tsx
@@ -5,42 +5,43 @@ import { USER_ROLES } from "../../lib/users/UserModel";
 import CategoryAdminDashboard from "./category";
 import UsersAdminDashboard from "./users";
 import FormationsAdminDashboard from "./formations";
+import Quizz from "./quizz";
 
 const SgAdmin = () => {
-  return (
-    <div className={"flex flex-col gap-3"}>
-      <UsersAdminDashboard />
-      <FormationsAdminDashboard />
-      <CategoryAdminDashboard />
-    </div>
-  );
+    return (
+        <div className={"flex flex-col gap-3"}>
+            <UsersAdminDashboard />
+            <FormationsAdminDashboard />
+            <CategoryAdminDashboard />
+            <Quizz />
+        </div>
+    );
 };
 
 SgAdmin.getLayout = function getLayout(page: ReactElement) {
-  return <AdminLayout>{page}</AdminLayout>;
+    return <AdminLayout>{page}</AdminLayout>;
 };
 
-
 export const getServerSideProps = withSessionSsr(async function getServerSideProps({ req }) {
-  if (req.session.user) {
-    const user = req.session.user;
+    if (req.session.user) {
+        const user = req.session.user;
 
-    if (user && user.role !== USER_ROLES.ADMIN) {
-      console.log("OUAIS JE SUIS LA");
-      return {
-        notFound: true
-      };
+        if (user && user.role !== USER_ROLES.ADMIN) {
+            console.log("OUAIS JE SUIS LA");
+            return {
+                notFound: true,
+            };
+        }
+        return {
+            props: {
+                user: req.session.user,
+            },
+        };
+    } else {
+        return {
+            notFound: true,
+        };
     }
-    return {
-      props: {
-        user: req.session.user
-      }
-    };
-  } else {
-    return {
-      notFound: true
-    };
-  }
 });
 
 export default SgAdmin;
